3f80cce36309a290417373ec7ab78beccead8d3d
bhavyachavda <bhavyachavda55@gmail.com>
2023-02-17 17:40:01 +0530

    first commit

---
+++ front/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import React, { useState, useEffect } from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Screen from './pages/Screen';
+import Listdata from './pages/Listdata';
+import Editdata from './pages/Editdata';
+import View from './pages/View';
+import Importdata from './pages/Importdata';
+
+function App() {
+  // const [message, setMessage] = useState("");
+
+  // useEffect(() => {
+  //   fetch("http://localhost:8000/message")
+  //     .then((res) => res.json())
+  //     .then((data) => setMessage(data.message));
+  // }, []);
+
+  return (
+    <div>
+      {/* <h1>{message}</h1> */}
+      <BrowserRouter>
+        <Routes>
+        <Route exact path='/' element={< Listdata />}></Route>
+        <Route exact path='/Screen' element={< Screen />}></Route>
+        <Route path="Edit" element={<Editdata/>}/>
+        <Route path="View" element={<View/>}/>
+        <Route path="Import" element={<Importdata/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
